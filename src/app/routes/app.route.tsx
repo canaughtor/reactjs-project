af2657b40b205615a97b25f04c2263b7953dd3b8
Anubhav M <canaughtor@gmail.com>
2022-10-11 16:49:11 +0530

    initialize redux store

--- src/app/routes/app.route.tsx
+++ src/app/routes/app.route.tsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from 'app/modules/home'
+import { Route, Routes } from 'react-router-dom';
+import Home from 'app/modules/home';
 
 export const AppRoutes = () => { return (
 <Routes>
